947ff5cb0e43d3acbf93e6072e162360a391c7c0
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2024-03-13 17:21:40 -0500

    caballeros1

--- src/app/components/CharacterCarousel.jsx
+++ src/app/components/CharacterCarousel.jsx
@@ -1,18 +1,55 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
-import Image from 'next/image';
-import characters from './data/characters.json';
-import styles from '../styles/index.module.scss';
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { wrap } from "popmotion";
+import Image from "next/image";
+import characters from "./data/characters.json";
+import styles from "../styles/index.module.scss";
+
+const variants = {
+  enter: (direction) => ({
+    x: direction > 0 ? 100 : -100,
+    opacity: 0,
+    scale: 0.7,
+    rotate: direction > 0 ? -50 : 50, 
+  }),
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+    scale: 1,
+    rotate: 0, 
+  },
+  exit: (direction) => ({
+    zIndex: 0,
+    x: direction < 0 ? 100 : -100,
+    opacity: 0,
+    scale: 0.7,
+    rotate: direction < 0 ? -50 : 50, 
+  }),
+};
+
+
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset, velocity) => Math.abs(offset) * velocity;
 
 const CharacterCarousel = () => {
   const [selectedCharacters, setSelectedCharacters] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [[page, direction], setPage] = useState([0, 0]);
+
+  const imageIndex = wrap(0, characters.length, page);
+
+  const paginate = (newDirection) => {
+    setPage([page + newDirection, newDirection]);
+  };
 
   const handleCharacterSelect = (id) => {
     if (selectedCharacters.includes(id)) {
-      setSelectedCharacters(selectedCharacters.filter((characterId) => characterId !== id));
+      setSelectedCharacters(
+        selectedCharacters.filter((characterId) => characterId !== id)
+      );
     } else {
       setSelectedCharacters([...selectedCharacters, id]);
     }
@@ -33,13 +70,30 @@ const CharacterCarousel = () => {
   return (
     <>
       <section className={styles.characterCarousel}>
-        <AnimatePresence initial={false} mode='wait'>
+        <AnimatePresence initial={false} mode="wait">
           <motion.figure
             key={currentIndex}
-            initial={{ x: '100%', opacity: 0, scale: 0.7 }}
-            animate={{ x: 0, opacity: 1, scale: 1 }}
-            exit={{ x: '-100%', opacity: 0, scale: 0.7 }}
-            transition={{ duration: 0.5 }}
+            custom={direction}
+            variants={variants}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            transition={{
+              x: { type: "spring", stiffness: 300, damping: 30 },
+              opacity: { duration: 0.2 },
+            }}
+            drag="x"
+            dragConstraints={{ left: 0, right: 0 }}
+            dragElastic={1}
+            onDragEnd={(e, { offset, velocity }) => {
+              const swipe = swipePower(offset.x, velocity.x);
+
+              if (swipe < -swipeConfidenceThreshold) {
+                paginate(1);
+              } else if (swipe > swipeConfidenceThreshold) {
+                paginate(-1);
+              }
+            }}
             className={styles.carouselItem}
           >
             <Image
@@ -47,6 +101,7 @@ const CharacterCarousel = () => {
               alt={characters[currentIndex].name}
               width={500}
               height={500}
+              priority={true}
             />
             <figcaption>{characters[currentIndex].name}</figcaption>
           </motion.figure>
@@ -58,10 +113,16 @@ const CharacterCarousel = () => {
         />
       </section>
       <nav className={styles.carouselControls}>
-        <button className={`${styles.carouselButton} ${styles.prevButton}`} onClick={handlePrevious}>
+        <button
+          className={`${styles.carouselButton} ${styles.prevButton}`}
+          onClick={handlePrevious}
+        >
           <span className={styles.goldButton}>Anterior</span>
         </button>
-        <button className={`${styles.carouselButton} ${styles.nextButton}`} onClick={handleNext}>
+        <button
+          className={`${styles.carouselButton} ${styles.nextButton}`}
+          onClick={handleNext}
+        >
           <span className={styles.goldButton}>Siguiente</span>
         </button>
       </nav>
